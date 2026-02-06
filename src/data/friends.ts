cbecd49ca24dfae88b6ed6b9d9dede7f2c887739
renleihaokun <chaogeshou520@163.com>
2026-02-06 18:32:41 +0800

    更新友链读取方式并修复ipv6地址显示问题

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -2,7 +2,6 @@
 // 用于管理友情链接页面的数据
 
 export interface FriendItem {
-	id: number;
 	title: string;
 	imgurl: string;
 	desc: string;
@@ -11,24 +10,11 @@ export interface FriendItem {
 }
 
 // 友情链接数据
-export const friendsData: FriendItem[] = [
-	{
-		id: 1,
-		title: "acofork",
-		imgurl: "https://avatars.githubusercontent.com/u/180811437?v=4",
-		desc: "二叉树树的博客",
-		siteurl: "https://blog.acofork.com/",
-		tags: ["blog"],
-	},
-	{
-		id: 2,
-		title: "摇月的魔法工坊",
-		imgurl: "https://YuzukiAtelier.com/head.webp",
-		desc: "记录了一些现代魔法的咏唱技法",
-		siteurl: "https://YuzukiAtelier.com",
-		tags: ["blog"],
-	},
-];
+// 使用 import.meta.glob 读取所有 .json 文件
+const allFriendJsonFiles = import.meta.glob<FriendItem>('./friends/*.json', { eager: true });
+
+// 将导入的 JSON 数据转换为 FriendItem 数组
+const friendsData: FriendItem[] = Object.values(allFriendJsonFiles).map(module => module.default);
 
 // 获取所有友情链接数据
 export function getFriendsList(): FriendItem[] {
